5fc8d3fc2f1d79662e1350eaf3343b9c3a16fd76
Mandeep Masoun <mandeepmasoun@yahoo.com>
2025-10-23 00:51:34 -0700

    emoji added

--- card/src/components/Footer.jsx
+++ card/src/components/Footer.jsx
@@ -1,11 +1,17 @@
+import Twitter from "/Users/mandeepmasoun/Desktop/MyFolders/website/card/card/src/images/Twitter Icon.png"
+import FB from "/Users/mandeepmasoun/Desktop/MyFolders/website/card/card/src/images/Facebook Icon.png"
+import Insta from "/Users/mandeepmasoun/Desktop/MyFolders/website/card/card/src/images/Instagram Icon.png"
+import Git from "/Users/mandeepmasoun/Desktop/MyFolders/website/card/card/src/images/GitHub Icon.png"
+
+
 export default function Footer() {
     return (
         <>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li><img src={Twitter} alt="twitter icon" /></li>
+                <li><img src={FB} alt=" fb icon" /></li>
+                <li><img src={Insta} alt="insta icon" /></li>
+                <li><img src={Git} alt="git icon" /></li>
             </ul>
         </>
     )

f2381400b0bd34f91542ead21c8f6c384b9902ff
Mandeep Masoun <mandeepmasoun@yahoo.com>
2025-10-24 00:59:56 -0700

    styles

--- card/src/components/Footer.jsx
+++ card/src/components/Footer.jsx
@@ -6,13 +6,13 @@ import Git from "/Users/mandeepmasoun/Desktop/MyFolders/website/card/card/src/im
 
 export default function Footer() {
     return (
-        <>
-            <ul>
-                <li><img src={Twitter} alt="twitter icon" /></li>
-                <li><img src={FB} alt=" fb icon" /></li>
-                <li><img src={Insta} alt="insta icon" /></li>
-                <li><img src={Git} alt="git icon" /></li>
+        <footer>
+            <ul className="links">
+                <li><a href="https://x.com"><img src={Twitter} alt="twitter icon" /></a></li>
+                <li><a href="https://facebook.com"><img src={FB} alt=" fb icon" /></a></li>
+                <li><a href="https://instagram.com"><img src={Insta} alt="insta icon" /></a></li>
+                <li><a href="https://github.com/Trusty22"><img src={Git} alt="git icon" /></a></li>
             </ul>
-        </>
+        </footer>
     )
 }

4183ba12898870e731c0d190a43fab1f26a5c1c4
Mandeep Masoun <mandeepmasoun@yahoo.com>
2025-10-24 19:42:31 -0700

    first sample business card proj done

--- card/src/components/Info.jsx
+++ card/src/components/Info.jsx
@@ -1,13 +1,18 @@
+import Email from "/Users/mandeepmasoun/Desktop/MyFolders/website/card/card/src/images/Mail.png"
+import Linkedin from "/Users/mandeepmasoun/Desktop/MyFolders/website/card/card/src/images/linkedin.png"
+
 export default function Info() {
     return (
         <>
-        
+
             <div className="interactables">
-                <button>Email</button>
-                <button>LinkedIn</button>
+                <button><img src={Email} />Email</button>
+                <button><img src={Linkedin} />LinkedIn</button>
             </div>
-            <h1>About</h1>
-            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <div className="content">
+                <h1>About</h1>
+                <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            </div>
         </>
     )
 }
--- card/src/components/Interests.jsx
+++ card/src/components/Interests.jsx
@@ -1,9 +1,11 @@
 export default function Interests() {
     return (
         <>
-            <h1>Interests</h1>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
-            </p>
+            <div className="content">
+                <h1>Interests</h1>
+                <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+                </p>
+            </div>
         </>
     )
 }
